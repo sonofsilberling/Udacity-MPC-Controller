6fe2b09c57e923c2f154cb570814aca27fd759dc
Robert <robert.silbiger@silberling.net>
2018-03-09 14:36:48 +0000

    First Commit

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -1,121 +1,403 @@
 #include "MPC.h"
-#include <cppad/cppad.hpp>
-#include <cppad/ipopt/solve.hpp>
-#include "Eigen-3.3/Eigen/Core"
+#include <sstream>
 
 using CppAD::AD;
+using std::vector;
+using namespace std;
 
-// TODO: Set the timestep length and duration
-size_t N = 0;
-double dt = 0;
 
-// This value assumes the model presented in the classroom is used.
-//
-// It was obtained by measuring the radius formed by running the vehicle in the
-// simulator around in a circle with a constant steering angle and velocity on a
-// flat terrain.
-//
-// Lf was tuned until the the radius formed by the simulating the model
-// presented in the classroom matched the previous radius.
-//
-// This is the length from front to CoG that has a similar radius.
-const double Lf = 2.67;
+void FG_polynomial::operator()(ADvector& fg, const ADvector& x) {
+    fg[0] = 0.0;
+    for (unsigned int j = 0; j < ptsx_.size(); j++) {
+        // Observed point
+        AD<double> y = 0;
+        // Evaluate polynomial for ptsx(j)
+        for (unsigned int i = 0; i < x.size(); i++) {
+            y = y + x[i] * CppAD::pow(ptsx_[j], i);
+        }
+        fg[0] = fg[0] + CppAD::pow((ptsy_[j] - y), 2);
+    }
+}
 
-class FG_eval {
- public:
-  // Fitted polynomial coefficients
-  Eigen::VectorXd coeffs;
-  FG_eval(Eigen::VectorXd coeffs) { this->coeffs = coeffs; }
+// Implement global kinematic model
+//x x_i=x_(i-1)+  cos⁡〖〖(psi〗_(i-1))* v_(i-1)*dt〗
+//y y_i=y_(i-1)+  sin⁡〖〖(psi〗_(i-1))* v_(i-1)*dt〗
+//v v_i=v_(i-1)+ a_(i-1)*dt
+//psi 〖psi〗_i=〖psi〗_(i-1)+v_(i-1)  *  d_(i-1)*dt/Lf
+//cte 〖cte〗_i=〖( polyeval(〗⁡〖x_(i-1))- y_(i-1)  )+〗  sin⁡〖〖(psi〗_(i-1))* v_(i-1)*dt〗
+//epsi  〖epsi〗_i=〖( 〖psi〗_(i-1 )- atan⁡polyeval'⁡〖x_(i-1) 〗 〗⁡〖 )+〗 v_(i-1)  *  d_(i-1)*dt/Lf
 
-  typedef CPPAD_TESTVECTOR(AD<double>) ADvector;
-  void operator()(ADvector& fg, const ADvector& vars) {
-    // TODO: implement MPC
-    // `fg` a vector of the cost constraints, `vars` is a vector of variable values (state & actuators)
-    // NOTE: You'll probably go back and forth between this function and
-    // the Solver function below.
-  }
-};
+ADvector FG_motion::globalKinematic(const ADvector& state, const ADvector& actuators, const double& dt) {
+    ADvector next_state(6);
 
-//
-// MPC class definition implementation.
-//
-MPC::MPC() {}
-MPC::~MPC() {}
 
-vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
-  bool ok = true;
-  size_t i;
-  typedef CPPAD_TESTVECTOR(double) Dvector;
+    // const auto px1_f = px0 + v0 * CppAD::cos(psi0) * dt;
+    // const auto py1_f = py0 + v0 * CppAD::sin(psi0) * dt;
+    // const auto psi1_f = psi0 + v0 * (-delta0) / Lf * dt;
+    // const auto v1_f = v0 + a0 * dt;
+    // const auto cte1_f = py_desired - py0 + v0 * CppAD::sin(epsi0) * dt;
+    // const auto epsi1_f = psi0 - psi_desired + v0 * (-delta0) / Lf * dt;
 
-  // TODO: Set the number of model variables (includes both states and inputs).
-  // For example: If the state is a 4 element vector, the actuators is a 2
-  // element vector and there are 10 timesteps. The number of variables is:
-  //
-  // 4 * 10 + 2 * 9
-  size_t n_vars = 0;
-  // TODO: Set the number of constraints
-  size_t n_constraints = 0;
+    next_state[IDX_x] = state[IDX_x] + state[IDX_v] * CppAD::cos(state[IDX_psi]) * dt;
+    next_state[IDX_y] = state[IDX_y] + state[IDX_v] * CppAD::sin(state[IDX_psi]) * dt;
+    next_state[IDX_v] = state[IDX_v] + actuators[IDX_a] * dt;
+    next_state[IDX_psi] = state[IDX_psi] - state[IDX_v] * actuators[IDX_delta] * dt / Lf;
 
-  // Initial value of the independent variables.
-  // SHOULD BE 0 besides initial state.
-  Dvector vars(n_vars);
-  for (int i = 0; i < n_vars; i++) {
-    vars[i] = 0;
-  }
+    // Predicted y coordinate and angle
+    AD<double> py = 0;
+    AD<double> ppsi = 0;
+    // Evaluate polynomial at x
+    for (unsigned int i = 0; i < coeffs_.size(); i++) {
+        py += coeffs_[i] * CppAD::pow(state[IDX_x], i);
+    }
+    // Evaluate Polynomial prime at x
+    for (unsigned int i = 1; i < coeffs_.size(); i++) {
+        ppsi += coeffs_[i] * CppAD::pow(state[IDX_x], i - 1) * i;
+    }
 
-  Dvector vars_lowerbound(n_vars);
-  Dvector vars_upperbound(n_vars);
-  // TODO: Set lower and upper limits for variables.
+    next_state[IDX_cte] = (py - state[IDX_y]) + (state[IDX_v] * CppAD::sin(state[IDX_epsi]) * dt);
+    next_state[IDX_epsi] = (state[IDX_psi] - ppsi) - state[IDX_v] * actuators[IDX_delta] * dt / Lf;
 
-  // Lower and upper limits for the constraints
-  // Should be 0 besides initial state.
-  Dvector constraints_lowerbound(n_constraints);
-  Dvector constraints_upperbound(n_constraints);
-  for (int i = 0; i < n_constraints; i++) {
-    constraints_lowerbound[i] = 0;
-    constraints_upperbound[i] = 0;
-  }
+    return next_state;
+}
 
-  // object that computes objective and constraints
-  FG_eval fg_eval(coeffs);
+void FG_motion::operator()(ADvector& fg, const ADvector& x) {
+    // Step 1: calculate Kinematic model
+    // initial state is in attribute state
+    // (to be optimized) actuators are in x
+    // x is set of deltas and set of acceleration
 
-  //
-  // NOTE: You don't have to worry about these options
-  //
-  // options for IPOPT solver
-  std::string options;
-  // Uncomment this if you'd like more print information
-  options += "Integer print_level  0\n";
-  // NOTE: Setting sparse to true allows the solver to take advantage
-  // of sparse routines, this makes the computation MUCH FASTER. If you
-  // can uncomment 1 of these and see if it makes a difference or not but
-  // if you uncomment both the computation time should go up in orders of
-  // magnitude.
-  options += "Sparse  true        forward\n";
-  options += "Sparse  true        reverse\n";
-  // NOTE: Currently the solver has a maximum time limit of 0.5 seconds.
-  // Change this as you see fit.
-  options += "Numeric max_cpu_time          0.5\n";
+    if (DEBUG) {
+        cout << "__FG_motion()__" << endl;
+        cout << "coefficients: " << coeffs_ << endl;
+        cout << "Initial state: " <<state_ << endl;
+        cout << "Initial actuators: " << actuators_ << endl;
+        cout << "x: " << x << endl;
+        cout << "fg: " << fg << endl;        
+        cout << x.size() << endl;
+        cout << fg.size() << endl;
+    }
 
-  // place to return solution
-  CppAD::ipopt::solve_result<Dvector> solution;
+    const unsigned int time_steps = N;
 
-  // solve the problem
-  CppAD::ipopt::solve<Dvector, FG_eval>(
-      options, vars, vars_lowerbound, vars_upperbound, constraints_lowerbound,
-      constraints_upperbound, fg_eval, solution);
+    ADvector current_state(6);
+    ADvector current_actuators(2);
+    ADvector previous_actuators(2);
+    ADvector next_state(6);
 
-  // Check some of the solution values
-  ok &= solution.status == CppAD::ipopt::solve_result<Dvector>::success;
+        // Cst to minimize
+    fg[0] = 0.0;
 
-  // Cost
-  auto cost = solution.obj_value;
-  std::cout << "Cost " << cost << std::endl;
+    // Initialize current state to initial state given
+    // Converting DVector to ADVector
+    for (unsigned int i=0; i<state_.size(); i++) {
+        current_state[i] = state_[i];
+    }
+    previous_actuators[IDX_delta] = actuators_[IDX_delta];
+    previous_actuators[IDX_a]     = actuators_[IDX_a];
 
-  // TODO: Return the first actuator values. The variables can be accessed with
-  // `solution.x[i]`.
-  //
-  // {...} is shorthand for creating a vector, so auto x1 = {1.0,2.0}
-  // creates a 2 element double vector.
-  return {};
+
+    for (unsigned int i = 0; i<time_steps; i++) {
+         // Get delta and acceleration from optimizer
+        current_actuators[IDX_delta] = x[i];
+        current_actuators[IDX_a]     = x[i + time_steps];
+
+        // determine next state based on current state and kinematic model
+        // next_state = globalKinematic(current_state, current_actuators, DT);
+        // relationship of current state + actuations and next state
+        // based on our kinematic model
+        next_state = globalKinematic(current_state, current_actuators, DT);
+
+        // Update Cost
+        // Penalize cross track error
+        fg[0] += w_cte     * CppAD::pow(next_state[IDX_cte] - ref_cte, 2);
+        // Penalize orientation
+        fg[0] += w_epsi    * CppAD::pow(next_state[IDX_epsi] - ref_epsi, 2);
+        // Penalize slow velocity
+        fg[0] += w_v       * CppAD::pow(next_state[IDX_v] - ref_v, 2);
+
+
+        // Penalize too much steering and acceleration
+        fg[0] += w_delta   * CppAD::pow(current_actuators[IDX_delta], 2);
+        fg[0] += w_a       * CppAD::pow(current_actuators[IDX_a], 2);
+
+        // Penalize change in steering and acceleration
+        fg[0] += w_delta_t * CppAD::pow(previous_actuators[IDX_delta] - current_actuators[IDX_delta], 2);
+        fg[0] += w_a_t     * CppAD::pow(previous_actuators[IDX_a] - current_actuators[IDX_a], 2);
+
+        // Set x coordinates
+        fg[1 + i] = next_state[IDX_x] - x[2*time_steps + i];
+        fg[1 + i + time_steps] = next_state[IDX_y] - x[3*time_steps + i];
+
+        // Repeat with new state
+        current_state = next_state;
+        previous_actuators = current_actuators;
+
+
+    }
+
+    if (DEBUG)
+        cout << "Cost #" << fg[0] << endl;
+
+
+}
+
+MPC::MPC() {
+
+    // Initialize coefficients
+    coeffs.resize(DEGREE_POLYNOMIAL);
+    coeffs_x_lowerbound.resize(DEGREE_POLYNOMIAL);
+    coeffs_x_upperbound.resize(DEGREE_POLYNOMIAL);
+    // No constraints for coefficients
+    for (unsigned int i = 0; i < coeffs.size(); i++) {
+        coeffs_x_lowerbound[i] = -1e19;
+        coeffs_x_upperbound[i] = 1e19;
+    }
+
+    //**************************************************************
+    //* SET INITIAL VALUES OF VARIABLES
+    //**************************************************************
+    x.resize(NX);
+    x_lowerbound.resize(NX);
+    x_upperbound.resize(NX);
+
+    // Steering limits set to 25 radians
+    for (int i = 0; i < N; i++) {
+        x[i] = 0;
+        x_lowerbound[i] = -0.436332;
+        x_upperbound[i] = 0.436332;
+    }
+    // throttle limits
+    for (int i = N; i < 2*N; i++) {
+        x[i] = 0.1; // Initialize to slight acceleration / thottle
+        x_lowerbound[i] = -1.0;
+        x_upperbound[i] = 1.0;
+    }    
+    // x and y limits
+    for (int i = 2*N; i < NX; i++) {
+        x[i] = 0.;
+        x_lowerbound[i] = -1e19;
+        x_upperbound[i] = 1e19;
+    }
+
+    g_lowerbound.resize(2*N);
+    g_upperbound.resize(2*N);
+    for (int i = 0; i < 2*N; i++) {
+        g_lowerbound[i] = 0;
+        g_upperbound[i] = 0;
+    }    
+
+    next_xs.resize(N);
+    next_ys.resize(N);
+    mpc_xs.resize(N);
+    mpc_ys.resize(N);
+
+    // object that computes objective and constraints
+    // Uncomment this if you'd like more print information
+    if (DEBUG)
+        options_model += "Integer print_level  5\n";
+    else
+        options_model += "Integer print_level  0\n";
+    // NOTE: Setting sparse to true allows the solver to take advantage
+    // of sparse routines, this makes the computation MUCH FASTER.
+    options_model += "Sparse  true        forward\n";
+    options_model += "Sparse  true        reverse\n";
+    // NOTE: Currently the solver has a maximum time limit of 0.5 seconds.
+    options_model += "Numeric max_cpu_time         30.0\n";    // object that computes objective and constraints
+    // Uncomment this if you'd like more print information
+    options_poly += "Integer print_level  0\n";
+    // NOTE: Setting sparse to true allows the solver to take advantage
+    // of sparse routines, this makes the computation MUCH FASTER.
+    options_poly += "Sparse  true        forward\n";
+    options_poly += "Sparse  true        reverse\n";
+    // NOTE: Currently the solver has a maximum time limit of 0.5 seconds.
+    options_poly += "Numeric max_cpu_time         30.0\n";
+}
+
+MPC::~MPC() {};
+
+void MPC::solve(const Dvector& state, const Dvector& actuators) {
+    if (DEBUG)
+        cout << "__solve__ enter" << endl;
+
+    if (LOG) {
+        log.open("logfile.txt",std::ios_base::app | std::ios_base::out);
+        log << "state:\n";
+        for (unsigned int i = 0;i<state.size();i++)
+            log << state[i] << "\n";
+        log << "actuators:\n";
+        for (unsigned int i = 0;i<actuators.size();i++)
+            log << actuators[i] << "\n";
+        log.close();
+    }
+
+    CppAD::ipopt::solve_result<Dvector> solution;
+    bool ok = true;
+
+    FG_motion fg_motion(coeffs, state, actuators);
+
+   // solve the problem
+    CppAD::ipopt::solve<Dvector, FG_motion>(
+     options_model,
+     x,
+     x_lowerbound,
+     x_upperbound,
+     g_lowerbound,
+     g_upperbound,
+     fg_motion,
+     solution);
+
+    auto cost = solution.obj_value;
+
+    ok &= solution.status == CppAD::ipopt::solve_result<Dvector>::success;
+
+    if (ok) {
+        cout << "OK! Cost: " << cost << endl;
+        if (DEBUG) {
+            cout << "OK! Solution x: " << solution.x << endl;
+            cout << "OK! Solution g: " << solution.g << endl;
+        }
+    }
+    else {
+        cout << "SOMETHING IS WRONG! " << solution.status << endl;
+    }
+
+    // To cater for latency, ignore first value at index 0
+    steer = solution.x[0];
+    throttle = solution.x[N + 0];
+
+    mpc_xs.clear();
+    mpc_ys.clear();
+    for (unsigned int i =0; i<N;i++) {
+        mpc_xs.push_back(solution.x[2*N + i]);
+        mpc_ys.push_back(solution.x[3*N + i]);
+    }
+
+    if (DEBUG)
+        cout << "Size of solution x: " << x.size() << endl;
+}
+
+void MPC::setPolynomialPoints() {
+
+    next_xs.clear();
+    next_ys.clear();
+
+    for (unsigned int i = 0; i < N; ++i) {
+        const double dx = D * i;
+        next_xs.push_back(dx);
+        next_ys.push_back(polyeval(dx));
+    }
+}
+
+
+// // Adapted from lecture notes:
+// // Estimate next state based on current state
+Dvector MPC::globalKinematic(const Dvector& state, const Dvector& actuators, double dt) {
+    // Create a new vector for the next state.
+    Dvector next_state(6);
+
+    next_state[IDX_x] = state[IDX_x] + state[IDX_v] * cos(state[IDX_psi]) * dt;
+    next_state[IDX_y] = state[IDX_y] + state[IDX_v] * sin(state[IDX_psi]) * dt;
+    next_state[IDX_v] = state[IDX_v] + actuators[IDX_a] * dt;
+    next_state[IDX_psi] = state[IDX_psi] - state[IDX_v] * actuators[IDX_delta] * dt / Lf;
+
+    // Predicted y coordinate and angle
+    double py = 0;
+    double ppsi = 0;
+    // Evaluate polynomial at x
+    for (unsigned int i = 0; i < coeffs.size(); i++) {
+        py += coeffs[i] * pow(state[IDX_x], i);
+    }
+    // Evaluate Polynomial prime at x
+    for (unsigned int i = 1; i < coeffs.size(); i++) {
+        ppsi += coeffs[i] * pow(state[IDX_x], i - 1) * i;
+    }
+
+    next_state[IDX_cte] = (py - state[IDX_y]) + (state[IDX_v] * sin(state[IDX_epsi]) * dt);
+    next_state[IDX_epsi] = (state[IDX_psi] - ppsi) - state[IDX_v] * actuators[IDX_delta] * dt / Lf;
+
+    return next_state;
+}
+
+// Fit polynomial to Waypoints
+// Reset to car's point of view
+
+void MPC::fitWaypoints(vector<double>& ptsx, vector<double>& ptsy) {
+    // Solve polynomial
+
+    CppAD::ipopt::solve_result<Dvector> solution;
+    bool ok = true;
+
+
+    // if (LOG) {
+    //     log.open("logfile.txt",std::ios_base::app | std::ios_base::out);
+    //     log << "ptsx:" << "\n";
+    //     for (unsigned int i=0;i<ptsx.size();i++)
+    //         log << ptsx[i] << "\n";
+    //     log << "ptsy:" << "\n";
+    //     for (unsigned int i=0;i<ptsx.size();i++)
+    //         log << ptsy[i] << "\n";
+    //     log.close();
+    // }
+
+    // Initialize values
+    for (unsigned int i = 0; i < coeffs.size(); i++) {
+        coeffs[i] = 1;
+    }
+    
+    // Map vector values to Devcor values
+    Dvector ptsx_d;
+    Dvector ptsy_d;
+    ptsx_d.resize(ptsx.size());
+    ptsy_d.resize(ptsy.size());
+    for (unsigned int i = 0; i<ptsx.size(); i++){
+        ptsx_d[i] = ptsx[i];
+        ptsy_d[i] = ptsy[i];
+    }
+    FG_polynomial fg_polynomial(ptsx_d, ptsy_d);
+
+    CppAD::ipopt::solve<Dvector, FG_polynomial>(
+        options_poly,
+        coeffs,
+        coeffs_x_lowerbound,
+        coeffs_x_upperbound,
+        coeffs_g_lowerbound,
+        coeffs_g_upperbound,
+        fg_polynomial,
+        solution);
+
+    auto cost = solution.obj_value;
+
+    ok &= solution.status == CppAD::ipopt::solve_result<Dvector>::success;
+    if (ok) {
+        cout << "OK! Polynomial Cost: " << cost << endl;
+        for (unsigned int i = 0; i < coeffs.size(); i++) {
+            coeffs[i] = solution.x[i];
+        }
+        if (DEBUG)
+            cout << coeffs << endl;
+
+    }
+    else {
+        cout << "SOMETHING IS WRONG in Polynomial ! " << solution.status << endl;
+    }
+
+    setPolynomialPoints();
+}
+
+// Evaluate a polynomial.
+double MPC::polyeval(double x) {
+    double result = 0.0;
+    for (unsigned int i = 0; i < coeffs.size(); i++) {
+        result += coeffs[i] * pow(x, i);
+    }
+    return result;
+}
+
+double MPC::polyeval_t(double x) {
+    double result = 0.0;
+    for (unsigned int i = 1; i < coeffs.size(); i++) {
+        result += coeffs[i] * pow(x, i - 1) * i;
+    }
+    return result;
 }
--- src/MPC.h
+++ src/MPC.h
@@ -1,20 +1,152 @@
 #ifndef MPC_H
 #define MPC_H
 
+#include <cppad/ipopt/solve.hpp>
 #include <vector>
-#include "Eigen-3.3/Eigen/Core"
+#include <ios>
+#include <fstream>
 
 using namespace std;
+using CppAD::AD;
+
+const bool DEBUG = false;
+const bool LOG = false;
+
+// Time between predictions ahead
+const double DT = 0.1; 
+const double Lf = 2.67;
+
+const int N = 10; // how many states we "lookahead" into the future
+
+const int NUMBER_OF_STATES = 6; // px, py, psi, v, cte, epsi
+const int NUMBER_OF_ACTUATORS = 2; // steering angle, acceleration
+
+const int DEGREE_POLYNOMIAL = 4;
+
+// number of state + actuation variables
+// EXCLUDING initial state
+// How many variables the optimizer has to concern itself with (x)
+// N steering values
+// N throttle values. AND
+// N x values - required for output
+// N y values - required for output but not strictly the optimizer
+const int NX = N * 4 ;
+// All input values for the optimizer
+const int NG = N * ( NUMBER_OF_STATES );
+
+// Index in State vector
+const int IDX_x = 0;
+const int IDX_y = 1;
+const int IDX_psi = 2;
+const int IDX_v = 3;
+const int IDX_cte = 4;
+const int IDX_epsi = 5;
+// Index in actuator vector
+const int IDX_delta = 0;
+const int IDX_a = 1;
+
+// Reference CTE, PSI, and target velocity
+const double ref_cte = 0;
+const double ref_epsi = 0;
+const double ref_v = 100;
+
+// Weights for cost function
+const double w_cte = 2000;
+const double w_epsi = 2000;
+const double w_v = 1;
+const double w_delta = 5;
+const double w_a = 1;
+const double w_delta_t = 200;
+const double w_a_t = 10;
+
+typedef CPPAD_TESTVECTOR(AD<double>) ADvector;
+typedef CPPAD_TESTVECTOR(double) Dvector;
+
+// IPopt functor for motion model
+class FG_motion {
+public:
+    typedef CPPAD_TESTVECTOR(AD<double>) ADvector;
+    // Coefficients of fitted polynomial curve
+    Dvector coeffs_;
+
+    // Initial (constant) state
+    Dvector state_;
+    Dvector actuators_;
+
+    FG_motion(Dvector coeffs_in, Dvector state_in, Dvector actuators_in) : coeffs_(coeffs_in), state_(state_in), actuators_(actuators_in) {
+    }
+
+    void operator()(ADvector& fg, const ADvector& x);
+
+    ADvector globalKinematic(const ADvector& state, const ADvector& actuators, const double& dt);
+};
+
+// IPopt functor for polynomial /curve fitting
+class FG_polynomial {
+public:
+    typedef CPPAD_TESTVECTOR(AD<double>) ADvector;
+    Dvector ptsx_;
+    Dvector ptsy_;
+
+    FG_polynomial(Dvector ptsx_in, Dvector ptsy_in) : ptsx_(ptsx_in), ptsy_(ptsy_in) {
+    }
+
+    void operator()(ADvector& fg, const ADvector& x);
+};
 
 class MPC {
- public:
-  MPC();
+public:
 
-  virtual ~MPC();
+    // Distance for predicted waypoints
+    const double D = 5.0;
 
-  // Solve the model given an initial state and polynomial coefficients.
-  // Return the first actuatotions.
-  vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+    std::ofstream log;
+
+    // Predicated Steer and Throttle values
+    double steer;
+    double throttle;
+
+    Dvector x; // where all the state and actuation variables will be stored
+    Dvector x_lowerbound; //lower limit for each corresponding variable in x
+    Dvector x_upperbound; //upper limit for each corresponding variable in x
+    Dvector g_lowerbound; // value constraint for each corresponding constraint expression
+    Dvector g_upperbound; // value constraint for each corresponding constraint expression
+
+    // Predicted waypoints - always N of those
+    vector<double> next_xs;
+    vector<double> next_ys;
+    // Predicted waypoints - always N of those
+    vector<double> mpc_xs;
+    vector<double> mpc_ys;
+
+    // Fitted polynomial coefficients
+    Dvector coeffs;
+    Dvector coeffs_x_lowerbound;
+    Dvector coeffs_x_upperbound;
+    Dvector coeffs_g_lowerbound;
+    Dvector coeffs_g_upperbound;
+
+    // options for IPOPT solver
+    std::string options_poly;
+    std::string options_model;
+
+    MPC();
+    virtual ~MPC();
+
+    Dvector globalKinematic(const Dvector& state, const Dvector& actuators, double dt);
+
+    void setPolynomialPoints();
+    void fitWaypoints(vector<double>& ptsx, vector<double>& ptsy);
+
+    // Evaluate Polynomial
+    double polyeval(double x);
+
+    // Evaluate first derivative of polynomial
+    double polyeval_t(double x);
+    //
+    // this function solves the model given the current state and road curve coefficients.
+    void solve(const Dvector& state, const Dvector& actuators);
+
 };
 
 #endif /* MPC_H */
